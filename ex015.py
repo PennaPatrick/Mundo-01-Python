4a12ddc8737a386282041e3dc37f423f3c0da4e8
Patrick <124536675+PennaPatrick@users.noreply.github.com>
2023-05-03 20:48:30 -0300

    Delete ex015.py

--- ex015.py
+++
@@ -1,6 +0,0 @@
-dias = int(input('Quantos dias alugados? '))
-km = int(input('Quantos km rodados? '))
-
-preço = (60 * dias) + (0.15 * km)
-
-print(f'O custo total dessa locação ficou no total de R${preço}')
